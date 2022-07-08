7df67272edfdda988503233590307e0757251a8c
buildmine10 <33100363+buildmine10@users.noreply.github.com>
2022-07-08 13:29:15 -0400

    Initialized repo

---
+++ src/main/java/port_a_vault/port_a_vault/Port_a_vault.java
@@ -0,0 +1,10 @@
+package port_a_vault.port_a_vault;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Port_a_vault implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/port_a_vault/port_a_vault/client/Port_a_vaultClient.java
@@ -0,0 +1,13 @@
+package port_a_vault.port_a_vault.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class Port_a_vaultClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
