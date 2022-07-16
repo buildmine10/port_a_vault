095e37919423d5e68ea4fcfcedd9aa40af8d31ba
buildmine10 <33100363+buildmine10@users.noreply.github.com>
2022-07-16 18:18:52 -0400

    Made custom chest

---
+++ src/main/java/port_a_vault/port_a_vault/blocks/CustomChest.java
@@ -0,0 +1,9 @@
+package port_a_vault.port_a_vault.blocks;
+
+import net.minecraft.block.Block;
+
+public class CustomChest extends Block {
+    public CustomChest(Settings settings) {
+        super(settings);
+    }
+}
