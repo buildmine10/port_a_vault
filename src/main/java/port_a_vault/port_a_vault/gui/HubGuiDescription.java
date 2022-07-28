a1723999b434c526b23eb7fb70353ddb33721417
jer-martin <48764226+jer-martin@users.noreply.github.com>
2022-07-28 14:24:29 -0400

    scroll box works in hub gui

--- src/main/java/port_a_vault/port_a_vault/gui/HubGuiDescription.java
+++ src/main/java/port_a_vault/port_a_vault/gui/HubGuiDescription.java
@@ -1,30 +1,37 @@
 package port_a_vault.port_a_vault.gui;
 
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription;
-import io.github.cottonmc.cotton.gui.widget.WGridPanel;
-import io.github.cottonmc.cotton.gui.widget.WItemSlot;
+import io.github.cottonmc.cotton.gui.widget.*;
+import io.github.cottonmc.cotton.gui.widget.data.Axis;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import io.github.cottonmc.cotton.gui.widget.data.VerticalAlignment;
+import io.github.cottonmc.cotton.gui.widget.icon.ItemIcon;
+import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Items;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.text.Text;
 import port_a_vault.port_a_vault.Port_a_vault;
 
 public class HubGuiDescription extends SyncedGuiDescription {
 
-    private static final int INVENTORY_SIZE = 1;
+    private static final int INVENTORY_SIZE = 27;
 
     public HubGuiDescription(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(Port_a_vault.HUB_SCREEN_HANDLER_TYPE , syncId, playerInventory, getBlockInventory(context, INVENTORY_SIZE), getBlockPropertyDelegate(context));
 
-        WGridPanel root = new WGridPanel();
-        setRootPanel(root);
-        root.setSize(300, 200);
-        root.setInsets(Insets.ROOT_PANEL);
+        WGridPanel root = (WGridPanel) rootPanel;
+        WBox box = new WBox(Axis.VERTICAL);
 
-        WItemSlot itemSlot = WItemSlot.of(blockInventory, 0);
-        root.add(itemSlot, 4, 1);
+        for (int i = 0; i < 20; i++) {
+            box.add(new WLabeledSlider(0, 10, Text.literal("Slider #" + i)));
+        }
 
-        root.add(this.createPlayerInventoryPanel(), 0, 3);
+        box.add(new WButton(new ItemIcon(Items.APPLE)));
+
+        root.add(new WLabel(Text.literal("Scrolling test")).setVerticalAlignment(VerticalAlignment.CENTER), 0, 0, 5, 2);
+        root.add(new WScrollPanel(box), 0, 2, 5, 3);
 
         root.validate(this);
     }
